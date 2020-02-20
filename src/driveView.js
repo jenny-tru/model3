000a2f7e1b94cd127d6725becdc627aae41082ab
Jenny <truong.a.jenny@gmail.com>
2020-02-19 22:19:18 -0500

    need to fix driver extra menu

--- src/driveView.js
+++ src/driveView.js
@@ -1,29 +1,82 @@
-// switch (state.status) {
-  //   case 'half':
-  //     switch (event.type) { // { type: 'CLICK' }
-  //       case 'CLICK':
-  //         return {
-  //           ...state,
-  //           status: 'full'
-  //         }
+import React, { useReducer } from 'react';
 
-  //       default:
-  //         return state; // nothing changes
-  //     }
+const machine = {
+    initial: 'half',
+    states: {
+        half: {
+            on: {
+                CLICK: 'full'
+            }
+        },
+        full: {
+            on: {
+                CLICK: 'half'
+            }
+        }
+    }
+}
 
-  //   case 'full':
-  //       switch (event.type) {
-  //         case 'CLICK':
-  //           return {
-  //             ...state,
-  //             status: 'half'
-  //           }
+const initialState = {
+    status: machine.initial
+}
 
-  //         default:
-  //           return state; // nothing changes
-  //       }
+// state + event = nextState
+function driveReducer(state, event) { // { type: 'CLICK' }
+    return {
+        ...state,
+        status: machine
+            .states[state.status] // what is the current status
+            .on[event.type] // what is the next status
+            || state.status
+    }
 
-  //   default:
-  //     // this will never be reached
-  //     return state; // nothing changes
-  // }
+    // switch (state.status) {
+    //   case 'half':
+    //     switch (event.type) { // { type: 'CLICK' }
+    //       case 'CLICK':
+    //         return {
+    //           ...state,
+    //           status: 'full'
+    //         }
+
+    //       default:
+    //         return state; // nothing changes
+    //     }
+
+    //   case 'full':
+    //       switch (event.type) {
+    //         case 'CLICK':
+    //           return {
+    //             ...state,
+    //             status: 'half'
+    //           }
+
+    //         default:
+    //           return state; // nothing changes
+    //       }
+
+    //   default:
+    //     // this will never be reached
+    //     return state; // nothing changes
+    // }
+}
+
+// active is true or false
+export function driveView({ active }) {
+    const [state, dispatch] = useReducer(musicReducer, initialState);
+
+    console.log(state); // { status: 'half' }
+
+    return (
+        <div
+            className="view"
+            data-active={active || undefined}
+            data-status={state.status}
+            onClick={(event) => {
+                // we don't care about the event
+                dispatch({ type: 'CLICK' })
+            }}>
+            <h1>Entertainment</h1>
+        </div>
+    )
+}
