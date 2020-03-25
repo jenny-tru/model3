e4ee355ee0a57f5338d8fb6af3fae0cd89353b56
Jenny <truong.a.jenny@gmail.com>
2020-03-25 16:17:36 -0400

    changes to fix ac page view

--- src/acView.js
+++ src/acView.js
@@ -16,37 +16,3 @@ const machine = {
     }
 }
 
-const initialState = {
-    status: machine.initial
-}
-
-function showAir(state, event) { 
-    return {
-        ...state,
-        status: machine
-            .states[state.status] // what is the current status
-            .on[event.type] // what is the next status
-            || state.status
-    }
-
-}
-
-
-/* export function acView({ active }) {
-    const [state, dispatch] = useReducer(showAir, initialState);
-
-    console.log(state); // { status: 'half' }
-
-    return (
-        <div
-            className="view"
-            data-active={active || undefined}
-            data-status={state.status}
-            onClick={(event) => {
-                // we don't care about the event
-                dispatch({ type: 'CLICK' })
-            }}>
-            <h1>AC View</h1>
-        </div>
-    )
-} */
