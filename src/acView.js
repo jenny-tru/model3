7480b15d2ec33d641e2250f37b1158e5480d9eb1
Jenny <truong.a.jenny@gmail.com>
2020-03-29 22:45:26 -0400

    changes made to ac view

--- src/acView.js
+++ src/acView.js
@@ -1,18 +1,26 @@
 import React, { useReducer } from 'react';
 
 const machine = {
-    initial: 'half',
-    states: {
-        half: {
-            on: {
-                CLICK: 'full'
-            }
-        },
-        full: {
-            on: {
-                CLICK: 'half'
-            }
-        }
+  initial: 'half',
+  states: {
+    half: {
+      on: {
+        CLICK: 'full'
+      }
+    },
+    full: {
+      on: {
+        CLICK: 'half'
+      }
     }
+  }
 }
 
+function musicReducer(state, event) { // { type: 'CLICK' }
+    return {
+        ...state,
+        status: machine
+            .states[state.status] // what is the current status
+            .on[event.type] // what is the next status
+            || state.status
+    }
