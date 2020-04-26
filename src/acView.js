efd45ec5e133a382eb312cc210566b73f695cdcf
Jenny <truong.a.jenny@gmail.com>
2020-04-26 00:00:05 -0400

    :tada:

--- src/acView.js
+++ src/acView.js
@@ -2,26 +2,6 @@ import React, {useReducer} from 'react';
 import { createMachine } from 'xstate';
 import { useMachine } from '@xstate/react';
 
-// const acMachine = createMachine({
-//     // replace full with open... everywhere
-//     initial: 'open',
-//     states: {
-//         open: {
-//             on:{
-//                 CLICK: 'hidden'
-//             }
-//             // do the same thing here
-//         },
-//         // let's add another state
-//         hidden: {
-//             on: {
-//                 CLICK: 'open'
-//             }
-//         }
-//     }
-// })
-
-
 // active (in our case) means...
 // it's the only active view
 // and we don't control that
